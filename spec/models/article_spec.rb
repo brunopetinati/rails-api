416a0798ba61617fdadf5ef7e29d5b384a15fd90
Winiwon1@Z <brunopetinati@hotmail.com>
2023-01-20 11:59:23 -0300

    correction and implementation for 3rd test

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,26 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  describe '#validations'
+  describe '#validations' do
+    let(:article) { build(:article) }
 
-  let(:article = build(:article) #criado por articles.rb em factories/)
-  # line 6 => article = FactoryBot.create(:article) be available
+    # let :article = build(:article) #criado por articles.rb em factories/)
+    # line 7 => article = FactoryBot.create(:article) be available
 
-  it 'tests article object' do
-    # article = Article.create(title:'sample article', content: 'this text area')
-    #expect(article.title).to eq('MyString')
-    expect(article).to be_valid # article.valid? == true
+    it 'tests article object' do
+      # article = Article.create(title:'sample article', content: 'this text area')
+      #expect(article.title).to eq('MyString')
+      expect(article).to be_valid # article.valid? == true
+    end
+
+
+    it 'has a valid title' do
+      expect(article.title).to eq('MyString')
+    end
+
+    it 'has an invalid title' do
+      article.title = ''
+      expect(article).not_to be_valid
+      expect(article.errors[:title]).to include("can't be blank")
+    end
   end
-
-
-  it 'has a valid title' do
-    expect(article.title).to eq('MyString')
-  end
-
-  it 'has an invalid title' do
-    article.title = ''
-    expect(article.title).not_to be_valid
-    expect(article.errors)[:title].to include("can't be blank")
-  end
-
 end
